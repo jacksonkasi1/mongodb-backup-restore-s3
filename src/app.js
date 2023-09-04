dc221e6e5fed021475c80441e11be60feac42fa5
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-09-04 17:10:56 +0530

    update api

--- src/app.js
+++ src/app.js
@@ -15,23 +15,220 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
 });
 
-// Promisify the exec function
-function promisifyExec(command) {
-  return new Promise((resolve, reject) => {
+// NORMAL WAY
+app.get("/api/backup", async (req, res) => {
+  try {
+    const { folder, dbUrl } = req.query;
+
+
+    const fileName = `backup_${Date.now()}.dump`; // Generate a unique file name
+    const filePath = path.join(__dirname, "../" + fileName);
+
+    console.log("Backup Process started...");
+
+    const command = `mongodump --forceTableScan --out=${filePath} --uri=${dbUrl}`;
+
+    exec(
+      command,
+      { maxBuffer: 1024 * 1024 * 5000 },
+      async (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return res.status(500).send(`exec error: ${error}`);
+        }
+
+
+        const data = await fs.promises.readFile(filePath);
+
+        const fileLocation = folder ? `${folder}/${fileName}` : fileName;
+
+        const params = {
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: fileLocation,
+          Body: data,
+          ContentType: "application/octet-stream",
+          ContentDisposition: `DB Backup file ${fileLocation} and time = ${Date.now()}`,
+        };
+
+        const uploadResult = await s3.upload(params).promise();
+
+        console.log(`Backup uploaded successfully to ${process.env.S3_BUCKET_NAME}/${fileName}`);
+        await fs.promises.unlink(filePath);
+        console.log("File deleted");
+
+        const location = uploadResult.Location;
+
+        res.status(200).json({
+          success: true,
+          message: `Backup uploaded successfully to ${location}`,
+          location,
+        });
+      }
+    );
+  } catch (err) {
+    console.error(`Error: ${err}`);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred",
+    });
+  }
+});
+
+app.get("/api/restore", async (req, res) => {
+  const { dbUrl, key } = req.query;
+
+  const fileName = `backup.dump`;
+  const filePath = path.join(__dirname, fileName);
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME, // Replace with your S3 bucket name
+    Key: key,
+  };
+
+  try {
+    // Download the backup file from S3
+    console.log("Downloading database backup file from S3...");
+    const response = await s3.getObject(params).promise();
+
+    if (!response.Body) {
+      console.log("Empty response body received from S3.");
+      return res.status(500).send("Empty response body received from S3.");
+    }
+
+    // Write the backup file to the local file system
+    await fs.writeFile(filePath, response.Body);
+    console.log("Database backup file downloaded successfully!");
+
+    // Restoring the database
+    console.log("Restoring the database...");
+
+    const command = `mongorestore --uri=${dbUrl} --drop --archive=${filePath}`;
+
+    exec(
+      command,
+      { maxBuffer: 1024 * 1024 * 5000 }, // Set maxBuffer option to 5000 MB (adjust as needed)
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return res.status(500).send(`Error restoring database: ${error}`);
+        }
+        console.log("Database restored!");
+        fs.unlinkSync(filePath); // Delete the backup file after restoration
+        res.send("Database restored successfully!");
+      }
+    );
+  } catch (err) {
+    console.error(`S3 download error: ${err}`);
+    res.status(500).send(`Error downloading from S3: ${err}`);
+  }
+});
+
+// GZIP WAY
+app.get("/api/backup_gz", async (req, res) => {
+  try {
+    const { folder, dbUrl } = req.query;
+
+
+    const fileName = `backup_${Date.now()}`; // Generate a unique file name
+    const filePath = path.join(__dirname, fileName);
+
+    console.log("Backup Process started...");
+
+    const command = `mongodump --archive=${filePath}.gz --gzip --uri=${dbUrl}`;
+
+    exec(
+      command,
+      { maxBuffer: 1024 * 1024 * 5000 },
+      async (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return res.status(500).send(`exec error: ${error}`);
+        }
+
+        const data = await fs.promises.readFile(`${filePath}.gz`);
+
+        const fileLocation = folder ? `${folder}/${fileName}` : fileName;
+
+        const params = {
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: fileLocation,
+          Body: data,
+          ContentType: "application/octet-stream",
+          ContentDisposition: `DB Backup file ${fileLocation} and time = ${Date.now()}`,
+        };
+
+        const uploadResult = await s3.upload(params).promise().then((data) => {
+          fs.promises.unlink(`${filePath}.gz`);
+          console.log("File deleted");
+          return data
+        })
+
+        console.log(`Backup uploaded successfully to ${process.env.S3_BUCKET_NAME}/${fileName}`);
+
+        const location = uploadResult.Location;
+
+        res.status(200).json({
+          success: true,
+          message: `Backup uploaded successfully to ${location}`,
+          location,
+        });
+      }
+    );
+  } catch (err) {
+    console.error(`Error: ${err}`);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred",
+    });
+  }
+});
+
+app.get("/api/restore-gz", async (req, res) => {
+  const { dbUrl, key } = req.query;
+
+  const fileName = `backup.gz`;
+  const filePath = path.join(__dirname, fileName);
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+  };
+
+  try {
+    console.log("Downloading database backup file from S3...");
+    const response = await s3.getObject(params).promise();
+
+    if (!response.Body) {
+      console.log("Empty response body received from S3.");
+      return res.status(500).send("Empty response body received from S3.");
+    }
+
+    await fs.promises.writeFile(filePath, response.Body);
+    console.log("Database backup file downloaded successfully!");
+
+    console.log("Restoring the database...");
+    const command = `mongorestore --uri=${dbUrl} --drop --gzip --archive=${filePath}`;
+
     exec(
       command,
       { maxBuffer: 1024 * 1024 * 5000 },
       (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
-          return reject(error);
+          return res.status(500).send(`Error restoring database: ${error}`);
         }
-        resolve();
+        console.log("Database restored!");
+        fs.unlinkSync(filePath);
+        res.send("Database restored successfully!");
       }
     );
-  });
-}
+  } catch (err) {
+    console.error(`S3 download error: ${err}`);
+    res.status(500).send(`Error downloading from S3: ${err}`);
+  }
+});
 
+// CHECK VERSION
 app.get("/api/mongodb-version", async (req, res) => {
   try {
     // Use the 'mongod' command with '--version' option to get the MongoDB version
@@ -80,103 +277,6 @@ app.get("/api/mongodb-version", async (req, res) => {
       success: false,
       message: "An error occurred while checking version information",
     });
-  }
-});
-
-
-app.get("/api/backup", async (req, res) => {
-  try {
-    const { folder, dbUrl } = req.query;
-
-    const fileName = `backup_${Date.now()}`;
-    const filePath = path.join(__dirname, fileName);
-
-    console.log("Backup Process started...");
-
-    const command = `mongodump --forceTableScan --out=${filePath} --uri=${dbUrl}`;
-    await promisifyExec(command); // Use a promisified version of exec
-
-    const data = await fs.readFile(filePath);
-
-    const fileLocation = folder ? `${folder}/${fileName}` : fileName;
-
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: fileLocation,
-      Body: data,
-      ContentType: "application/octet-stream",
-      ContentDisposition: `DB Backup file ${fileLocation} and time = ${Date.now()}`,
-    };
-
-    const uploadResult = await s3.upload(params).promise();
-
-    console.log(`Backup uploaded successfully to ${bucket}/${fileName}`);
-    await fs.unlink(filePath);
-    console.log("File deleted");
-
-    const location = uploadResult.Location; // Get the S3 object location
-
-    res.status(200).json({
-      success: true,
-      message: `Backup uploaded successfully to ${location}`,
-      location, // Shorthand property name
-    });
-  } catch (err) {
-    console.error(`Error: ${err}`);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred",
-    });
-  }
-});
-
-
-app.get("/api/restore", async (req, res) => {
-  const { dbUrl, key } = req.query;
-
-  const fileName = `backup.sql`;
-  const filePath = path.join(__dirname, fileName);
-
-  const params = {
-    Bucket: process.env.S3_BUCKET_NAME, // Replace with your S3 bucket name
-    Key: key,
-  };
-
-  try {
-    // Download the backup file from S3
-    console.log("Downloading database backup file from S3...");
-    const response = await s3.getObject(params).promise();
-
-    if (!response.Body) {
-      console.log("Empty response body received from S3.");
-      return res.status(500).send("Empty response body received from S3.");
-    }
-
-    // Write the backup file to the local file system
-    await fs.writeFile(filePath, response.Body);
-    console.log("Database backup file downloaded successfully!");
-
-    // Restoring the database
-    console.log("Restoring the database...");
-
-    const command = `mongorestore --uri=${dbUrl} --drop --archive=${filePath}`;
-
-    exec(
-      command,
-      { maxBuffer: 1024 * 1024 * 5000 }, // Set maxBuffer option to 5000 MB (adjust as needed)
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return res.status(500).send(`Error restoring database: ${error}`);
-        }
-        console.log("Database restored!");
-        fs.unlinkSync(filePath); // Delete the backup file after restoration
-        res.send("Database restored successfully!");
-      }
-    );
-  } catch (err) {
-    console.error(`S3 download error: ${err}`);
-    res.status(500).send(`Error downloading from S3: ${err}`);
   }
 });
 
